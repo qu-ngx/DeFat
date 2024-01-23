1dedd8f5daba2c5e9f663e5403a8158471241b26
qu-ngx <quang13012005@gmail.com>
2024-01-23 13:23:41 -0800

    added UI login page

--- lib/pages/authenticator/login_page.dart
+++ lib/pages/authenticator/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:defat/components/my_textfield.dart';
+import 'package:defat/components/login_button.dart';
+import 'package:defat/components/square_tile.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -7,13 +9,16 @@ class LoginPage extends StatelessWidget {
   final usernameController = TextEditingController();
   final passwordController = TextEditingController();
 
+  void signUserIn() {}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.orange[100],
       body: SafeArea(
           child: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const SizedBox(height: 50),
             // Logo
@@ -35,13 +40,85 @@ class LoginPage extends StatelessWidget {
               obscureText: false,
             ),
 
-            const SizedBox(height: 15),
+            const SizedBox(height: 10),
 
             // password textfield
             MyTextField(
               controller: passwordController,
               hintText: 'Password',
               obscureText: true,
+            ),
+
+            // draw forgot password text
+            const SizedBox(height: 8),
+
+            // forgot the password
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "Forgot Password?",
+                    style: TextStyle(color: Colors.grey[600]),
+                  )
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 20),
+
+            // Sign-in button
+            SignInButton(onTap: signUserIn),
+
+            const SizedBox(height: 40),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text('Or continue with',
+                        style: TextStyle(color: Colors.grey[700])),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 25),
+            // Google sign in symbol (to be added features)
+
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [SquareTile(imagePath: 'assets/icons/google.png')],
+            ),
+            const SizedBox(height: 30),
+
+            // not a member? register text button
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Not a member?"),
+                SizedBox(width: 4),
+                Text(
+                  "Register Now",
+                  style: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.bold),
+                )
+              ],
             )
           ],
         ),
