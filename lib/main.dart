84c5f624e964d20669d21dfec8d1b2e0d61f2dec
qu-ngx <quang13012005@gmail.com>
2024-01-22 23:51:09 -0800

    added text field component for login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
-import 'package:defat/pages/home.dart';
+import 'package:defat/pages/authenticator/login_page.dart';
+// import 'package:defat/pages/home.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'pages/authenticator/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: LoginPage(),
     );
   }
 }
