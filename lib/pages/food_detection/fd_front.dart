ba1c41aecbf984acf71d190e1d8fcf0fbdfb4907
qu-ngx <quang13012005@gmail.com>
2024-01-29 23:13:23 -0500

    merged to resolve conflicts

--- lib/pages/food_detection/fd_front.dart
+++ lib/pages/food_detection/fd_front.dart
@@ -19,34 +19,33 @@ class _FoodMLState extends State<FoodML> {
   FoodDetection? foodDetection;
 
   Uint8List? image;
-  String? label;
 
   // This widgetList is a children for line 143
   List<Widget> widgetList = [];
 
-
   // Replace the according food name widget with a more detailed information one
   Widget replaceWidget() {
-     return Container(
+    return Container(
       color: Colors.amber,
       height: 120,
       width: 200,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget> [
+        children: <Widget>[
           Text("Food header with info"),
-          IconButton(onPressed: () {
-            setState(() {
-              // 0 is the index of the widget you want to replace
-              // 1 is 0 + 1 because you just want to replace that one widget
-              widgetList.replaceRange(0, 1, [initWidget()]);
-            });
-          },
+          IconButton(
+            onPressed: () {
+              setState(() {
+                // 0 is the index of the widget you want to replace
+                // 1 is 0 + 1 because you just want to replace that one widget
+                widgetList.replaceRange(0, 1, [initWidget()]);
+              });
+            },
             icon: Icon(Icons.arrow_drop_down_circle_outlined),
           )
         ],
       ),
-    ); 
+    );
   }
 
   // The default food name widget
@@ -57,15 +56,16 @@ class _FoodMLState extends State<FoodML> {
       width: 200,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget> [
+        children: <Widget>[
           Text("Food header"),
-          IconButton(onPressed: () {
-            setState(() {
-              // 0 is the index of the widget you want to replace
-              // 1 is 0 + 1 because you just want to replace that one widget
-              widgetList.replaceRange(0, 1, [replaceWidget()]);
-            });
-          },
+          IconButton(
+            onPressed: () {
+              setState(() {
+                // 0 is the index of the widget you want to replace
+                // 1 is 0 + 1 because you just want to replace that one widget
+                widgetList.replaceRange(0, 1, [replaceWidget()]);
+              });
+            },
             icon: Icon(Icons.arrow_drop_down_circle_outlined),
           )
         ],
@@ -73,6 +73,7 @@ class _FoodMLState extends State<FoodML> {
     );
   }
 
+  String? label;
 
   @override
   void initState() {
@@ -83,127 +84,56 @@ class _FoodMLState extends State<FoodML> {
 
   @override
   Widget build(BuildContext context) {
-    final planner = context.read<Planner>();
-    final foodList = planner.foodList;
     return Scaffold(
-<<<<<<< HEAD
-        body: SafeArea(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: (image != null) ? Image.memory(image!) : Container(),
-          ),
-          SizedBox(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                if (Platform.isAndroid || Platform.isIOS)
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: (image != null) ? Image.memory(image!) : Container(),
+              ),
+            ),
+            SizedBox(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  if (Platform.isAndroid || Platform.isIOS)
+                    IconButton(
+                      onPressed: () async {
+                        final result = await imagePicker.pickImage(
+                          source: ImageSource.camera,
+                        );
+                        if (result != null) {
+                          image = foodDetection!.analyseImage(result.path);
+                          setState(() {});
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.camera,
+                        size: 64,
+                      ),
+                    ),
                   IconButton(
                     onPressed: () async {
                       final result = await imagePicker.pickImage(
-                        source: ImageSource.camera,
+                        source: ImageSource.gallery,
                       );
                       if (result != null) {
                         image = foodDetection!.analyseImage(result.path);
-
-                        // Get label and return a small column leading to Dictionary or adding to meal planner
-                        label = foodDetection!.getPredLabel(result.path);
-                        int foodIndex;
-                        for (int i = 0; i < foodList.length; i++) {
-                          if (label == foodList[i].label) {
-                            foodIndex = i;
-                          }
-                        }
                         setState(() {});
                       }
                     },
                     icon: const Icon(
-                      Icons.camera,
+                      Icons.photo,
                       size: 64,
                     ),
-=======
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Column(
-              children: <Widget>[
-                Expanded(
-                  child: Center(
-                    child: (image != null) ? Image.memory(image!) : Container(),
                   ),
-                ),
-                SizedBox(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      if (Platform.isAndroid || Platform.isIOS)
-                        IconButton(
-                          onPressed: () async {
-                            final result = await imagePicker.pickImage(
-                              source: ImageSource.camera,
-                            );
-                            if (result != null) {
-                              image = foodDetection!.analyseImage(result.path);
-                              setState(() {});
-                            }
-                          },
-                          icon: const Icon(
-                            Icons.camera,
-                            size: 64,
-                          ),
-                        ),
-                      IconButton(
-                        onPressed: () async {
-                          final result = await imagePicker.pickImage(
-                            source: ImageSource.gallery,
-                          );
-                          if (result != null) {
-                            image = foodDetection!.analyseImage(result.path);
-                            setState(() {});
-                          }
-                        },
-                        icon: const Icon(
-                          Icons.photo,
-                          size: 64,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-            Container(
-              child: Stack(
-                children: [
-                  Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: widgetList,
-                      )
-                    ],
->>>>>>> e3ffa4316640f80f973959cd7f78420dd4738e5b
-                  ),
-                IconButton(
-                  onPressed: () async {
-                    final result = await imagePicker.pickImage(
-                      source: ImageSource.gallery,
-                    );
-                    if (result != null) {
-                      image = foodDetection!.analyseImage(result.path);
-                      setState(() {});
-                    }
-                  },
-                  icon: const Icon(
-                    Icons.photo,
-                    size: 64,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
